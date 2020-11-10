8a6c0a46a649cc7d532d983bf393fe6fda5eac45
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-11-09 19:34:13 -0700

    Update vocab.vue

--- src/views/vocab.vue
+++ src/views/vocab.vue
@@ -53,3 +53,23 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+input {
+  font-size: 1.2em;
+  height: 30px;
+  width: 200px;
+}
+
+textarea {
+  font-size: 1.6em;
+  width: 100%;
+  max-width: 500px;
+  height: 100px;
+}
+
+button {
+  margin-top: 20px;
+  font-size: 1.2em;
+}
+</style>
