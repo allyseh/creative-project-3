bce302c0247cf9e80f16f591eaa333c8bf6b925a
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-11-09 21:15:56 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="home">
-    <h1>Vocabulary Review for Midterm 2</h1>
+    <h1>Related Article Links</h1>
     <div v-for="article in links" v-bind:key="article.id">
       <hr>
       <div class="articles">
         <div class="article">
-          <p> Test ({{article.id}} </p>
           <a v-bind:href=article.link>{{article.title}}</a>
         </div>
       </div>
