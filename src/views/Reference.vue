09e7cb7195959e0b1f54d33ac55f804c1503a8e7
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-11-09 23:23:22 -0700

    Update Reference.vue

--- src/views/Reference.vue
+++ src/views/Reference.vue
@@ -8,31 +8,39 @@
     <h1>Formulas</h1>
     <div class="formulas">
       <img src="../../imgs/corrections.png">
-      <p class="formula"> Recombination Frequency (RF) = total number recombinants / total number gametes </p>
-      <p class="formula"> # non-recombinants always less >= # recombinants </p>
-      <p class="formula">	1 cM = 1% recombination frequency </p>
-      <p class="formula"> RF flanking genes = RF1 + RF2 </p>
-      <p class="formula">	Expected # DCOs = RF1 x RF2 x tot. # individuals </p>
-      <p class="formula"> Coefficient of Coincidence (C) = observed DCOs / expected DCOs </p>
-      <p class="formula">	Interference (I) = 1 - C </p>
+      <p class="left"> Recombination Frequency (RF) </p> <p> = <i>total number recombinants / total number gametes</i> </p>
+      <p class="left"> # non-recombinants always </p> <p> >= <i># recombinants</i> </p>
+      <p class="left"> 1 cM </p> <p> = <i>1% recombination frequency</i> </p>
+      <p class="left"> RF flanking genes </p> <p> = <i>RF1 + RF2</i> </p>
+      <p class="left"> Expected # DCOs </p> <p> = <i>RF1 x RF2 x tot. # individuals</i> </p>
+      <p class="left"> Coefficient of Coincidence (C) </p> <p> = <i>observed DCOs / expected DCOs</i> </p>
+      <p class="left"> Interference (I) </p> <p> = <i>1 - C</i> </p>
     </div>
     <h1>Ratios</h1>
     <div class="ratios">
-      <p class="ratio">	Reciprocal Translocation => 2 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p>
-      <p class="ratio"> Robertsonian Translocation => 4 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p>
-      <p class="ratio"> Trisomic Inheritance => 1 (AA) : 2 (Aa) : 2 (A) : 1 (a) </p>
-      <p class="ratio"> Autopolyploidy Inheritance => 1 (AA) : 4 (Aa) : 1 (aa) </p>
+      <p class="left"> Reciprocal Translocation </p> <p class="right"> => 2 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p>
+      <p class="left"> Robertsonian Translocation </p> <p class="right"> => 4 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p>
+      <p class="left"> Trisomic Inheritance </p> <p class="right"> => 1 (AA) : 2 (Aa) : 2 (A) : 1 (a) </p>
+      <p class="left"> Autopolyploidy Inheritance </p> <p class="right"> => 1 (AA) : 4 (Aa) : 1 (aa) </p>
     </div>
   </div>
 </template>
 
 <style>
-.recombination, .formulas, .rations {
+.recombination, .formulas, .ratios {
   margin-left: 20px;
   margin-bottom: 50px;
 }
 
 p {
   font-size: 24px;
+}
+
+.left {
+  font-weight: bold;
+}
+
+.right {
+  font-style: italic;
 }
 </style>
