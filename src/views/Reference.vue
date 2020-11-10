503725fbd1c1e2a05c3625c73bb1d9499b6366d6
Allyse Hettinger <51002304+allyseh@users.noreply.github.com>
2020-11-09 23:33:15 -0700

    Update Reference.vue

--- src/views/Reference.vue
+++ src/views/Reference.vue
@@ -8,20 +8,20 @@
     <h1>Formulas</h1>
     <div class="formulas">
       <img src="../../imgs/corrections.png">
-      <div class="line"> <p class="left"> Recombination Frequency (RF)   </p> <p> = <i>total number recombinants / total number gametes</i> </p> </div>
-      <div class="line"> <p class="left"> # non-recombinants   </p> <p> always >= <i># recombinants</i> </p> </div>
-      <div class="line"> <p class="left"> 1 cM   </p> <p> = <i>1% recombination frequency</i> </p> </div>
-      <div class="line"> <p class="left"> RF flanking genes   </p> <p> = <i>RF1 + RF2</i> </p> </div>
-      <div class="line"> <p class="left"> Expected # DCOs   </p> <p> = <i>RF1 x RF2 x tot. # individuals</i> </p> </div>
+      <div class="line"> <p class="left"> Recombination Frequency (RF) </p> <p> = <i>total number recombinants / total number gametes</i> </p> </div>
+      <div class="line"> <p class="left"> # non-recombinants  </p> <p> always >= <i># recombinants</i> </p> </div>
+      <div class="line"> <p class="left"> 1 cM  </p> <p> = <i>1% recombination frequency</i> </p> </div>
+      <div class="line"> <p class="left"> RF flanking genes  </p> <p> = <i>RF1 + RF2</i> </p> </div>
+      <div class="line"> <p class="left"> Expected # DCOs  </p> <p> = <i>RF1 x RF2 x tot. # individuals</i> </p> </div>
       <div class="line"> <p class="left"> Coefficient of Coincidence (C)   </p> <p> = <i>observed DCOs / expected DCOs</i> </p> </div>
-      <div class="line"> <p class="left"> Interference (I)   </p> <p> = <i>1 - C</i> </p> </div>
+      <div class="line"> <p class="left"> Interference (I)  </p> <p> = <i>1 - C</i> </p> </div>
     </div>
     <h1>Ratios</h1>
     <div class="ratios">
-      <div class="line"> <p class="left"> Reciprocal Translocation   </p> <p class="right"> => 2 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p> </div>
-      <div class="line"> <p class="left"> Robertsonian Transl.   </p> <p class="right"> => 4 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p> </div>
-      <div class="line"> <p class="left"> Trisomic Inheritance   </p> <p class="right"> => 1 (AA) : 2 (Aa) : 2 (A) : 1 (a) </p> </div>
-      <div class="line"> <p class="left"> Autopolyploidy Inheritance   </p> <p class="right"> => 1 (AA) : 4 (Aa) : 1 (aa) </p> </div>
+      <div class="line"> <p class="left"> Reciprocal Translocation  </p> <p class="right"> => 2 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p> </div>
+      <div class="line"> <p class="left"> Robertsonian Transl.  </p> <p class="right"> => 4 unbalanced : 1 balanced w/ translocation : 1 balanced w/o translocation </p> </div>
+      <div class="line"> <p class="left"> Trisomic Inheritance  </p> <p class="right"> => 1 (AA) : 2 (Aa) : 2 (A) : 1 (a) </p> </div>
+      <div class="line"> <p class="left"> Autopolyploidy Inheritance  </p> <p class="right"> => 1 (AA) : 4 (Aa) : 1 (aa) </p> </div>
     </div>
   </div>
 </template>
@@ -38,6 +38,7 @@ p {
 
 .left {
   font-weight: bold;
+  margin-right: 5px;
 }
 
 .right {
